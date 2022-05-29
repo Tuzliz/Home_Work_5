8a9d71c1b7fddb793e9938ec4e3b952ce3ab67c1
liza <lizashatuzhilkina@gmail.com>
2022-05-29 14:35:08 +0300

    change

--- 34/Program.cs
+++ 34/Program.cs
@@ -3,54 +3,44 @@
 //345, 897, 568, 234] -> 2
 
 
-int[] arrayCreation(int length)
-{
-    int[] arr = new int[length];
-    Random rnd = new Random();
-    for ( int i = 0 ; i< length ; i ++)
-    {
-        arr[i] = rnd.Next(100,999); 
-    }
-return arr;}
-
-Console.Write("Размер массива : ");
-int length = int.Parse(Console.ReadLine());
-int[] array = arrayCreation(length);
-string PrintArray(int[] array)
-{
-    string result = String.Empty;
-    for (int i = 0; i < array.Length; i++){
-        if (i == 0) result = "[" + Convert.ToString(array[i]) + ", ";//string builder
-        else if (i < array.Length-1)
-            result = result + Convert.ToString(array[i]) + ", ";
-        else result = result + Convert.ToString(array[i]) + "]";
-    }
-return result;
-}
-
-int GetEvenArray (int [] array) 
-{    
-    int k = 0;  
+//метод создания массива с рандомными значениями
+int[] arrayCreation(int length) 
+{ 
+    int[] arr = new int[length]; 
+    Random rnd = new Random(); 
+    for ( int i = 0 ; i< length ; i ++) 
+    { 
+        arr[i] = rnd.Next(100,999);  
+    } 
+return arr;} 
  
-    foreach(int element in array) 
-            { 
- 
-                Console.Write("{0} ", element); 
-                if (element % 2 == 0) 
-                { 
-                    k++; 
-                     
-                } 
-            } 
-    return k;         
- 
- 
+// вывод в консоль массива
+string PrintArray(int[] array) 
+{ 
+    string result = String.Empty; 
+    for (int i = 0; i < array.Length; i++){ 
+        if (i == 0) result = "[" + Convert.ToString(array[i]) + ", ";//string builder 
+        else if (i < array.Length-1) 
+            result = result + Convert.ToString(array[i]) + ", "; 
+        else result = result + Convert.ToString(array[i]) + "]"; 
+    } 
+return result; 
 } 
+ 
+ //метод подсчета количество чётных чисел
 
-Console.WriteLine($"{PrintArray(array)} -> {GetEvenArray(array)}"); 
-
-
-
-
-
-
+int GetEvenArray (int [] array)  
+{     
+    int k = 0;   
+    foreach(int element in array)  {  
+    if (element % 2 == 0) {  
+            k++;  
+            }  
+    }  
+    return k;
+}  
+ 
+Console.Write("Размер массива : "); 
+int length = int.Parse(Console.ReadLine()); 
+int[] array = arrayCreation(length);
+Console.WriteLine($"{PrintArray(array)} -> {GetEvenArray(array)}");
